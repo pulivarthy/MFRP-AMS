cd65da71dcd0b3f89e39fa3173823b3a92553a22
pulivarthy <31721584+pulivarthy@users.noreply.github.com>
2019-03-26 17:55:24 +0530

    Add files via upload

---
+++ Agent_CommissionBLL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DEL;
+namespace BLL
+{
+    public class Agent_CommissionBLL
+    {
+        Agent_Commission ad = new Agent_Commission();
+        IDAL<Agent_Commission> addal = new Agent_CommissionDAL();
+        public bool Save(Agent_Commission ad)
+        {
+            return addal.Save(ad);
+        }
+        public Agent_Commission GetAgent_CommissionInfoById(int Login_Id)
+        {
+            return addal.GetById(Login_Id);
+        }
+        public bool Agent_CommissionSpUpd( Agent_Commission um)
+        {
+            return addal.Update(um);
+        }
+
+        //public List<Admin> GetAll()
+        //{
+        //    return ad.GetAll();
+        //}
+    }
+}
---
+++ License_ManagementBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DEL;
+namespace BLL
+{
+    public class License_ManagementBLL
+    {
+        License_Management ad = new License_Management();
+        IDAL<License_Management> addal = new License_ManagementDAL();
+        public bool Save(License_Management ad)
+        {
+            return addal.Save(ad);
+        }
+        public License_Management GetLicense_ManagementInfoById(int Login_Id)
+        {
+            return addal.GetById(Login_Id);
+        }
+        public bool License_Managementsspupd(License_Management um)
+        {
+            return addal.Update(um);
+        }
+
+        //public List<Admin> GetAll()
+        //{
+        //    return ad.GetAll();
+        //}
+    }
+}
+
